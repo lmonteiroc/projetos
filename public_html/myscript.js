639d67acaede72ad9fe43ca5bf6b948ed9ce75a5
Luciano <77593045+lmonteiroc@users.noreply.github.com>
2021-07-31 09:20:31 -0300

    Add files via upload

---
+++ public_html/myscript.js
@@ -0,0 +1,32 @@
+
+function aumenta(obj){
+    obj.height=obj.height*2;
+	obj.width=obj.width*2;
+}
+ 
+function diminui(obj){
+	obj.height=obj.height/2;
+	obj.width=obj.width/2;
+}
+
+function trocar(){
+	document.getElementById("mousemove").innerHTML="Procuramos despertar o melhor nas pessoas desenvolvendo algo que seja útil e motivador, -'Pensamentos geram sentimentos que geram ações, ações geram resultados, vivemos em um mundo de casusa e efeito T. Have Eker'"
+}
+
+function voltar(){
+	document.getElementById("mousemove").innerHTML="I help you to design your ideal product."
+}
+function funcaoChange(elemento){
+console.log(elemento.value)
+}
+function getValue(){
+	var value = document.getElementById('sample').value;alert(value);
+}
+
+/*var um ="cursos no linketree";
+var dois ="veja também nosso";
+var tres = "material gratuito!";
+alert("Conheça nossos"+ um + dois + tres)*/
+
+
+  
